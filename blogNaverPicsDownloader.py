873b69173467791396d5e61d3979b3f4bfbcc03c
NiceFlight <167335404+NiceFlight@users.noreply.github.com>
2025-03-17 16:10:21 +0800

    Update blogNaverPicsDownloader.py

--- blogNaverPicsDownloader.py
+++ blogNaverPicsDownloader.py
@@ -4,8 +4,8 @@ import time
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
-HEADERS = {"Cookie": "over18=1",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; "
+HEADERS = {
+            User-Agent": "Mozilla/5.0 (Windows NT 10.0; "
                             "Win64; x64) AppleWebKit/537.36 "
                             "(KHTML, like Gecko) "
                             "Chrome/126.0.0.0 Safari/537.36"
