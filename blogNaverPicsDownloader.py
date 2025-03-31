3fee6aff8dd0b781b6950be4b389061463f0902b
NiceFlight <167335404+NiceFlight@users.noreply.github.com>
2025-03-31 10:32:17 +0800

    Add files via upload

--- blogNaverPicsDownloader.py
+++ blogNaverPicsDownloader.py
@@ -1,53 +1,98 @@
 import os
+import re
 import requests
 import time
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
+from urllib.parse import unquote
 
 HEADERS = {
-            User-Agent": "Mozilla/5.0 (Windows NT 10.0; "
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; "
                             "Win64; x64) AppleWebKit/537.36 "
                             "(KHTML, like Gecko) "
                             "Chrome/126.0.0.0 Safari/537.36"
             }
 
-url = input('Enter the url you want to download: ')
-options = webdriver.ChromeOptions()
-options.add_argument('--no-sandbox')
-driver = webdriver.Chrome(options=options)
+def Iniwebdriver():
+    options = webdriver.ChromeOptions()
+    options.add_argument('--no-sandbox')
+    options.add_argument('--headless')  # disable UI
+    options.add_argument('--disable-gpu')  # disable GPU, usually work with --no-headless
+    driver = webdriver.Chrome(options=options)
+    return driver
+
+# find out how many photos 
+def Countsofphotos(driver, url):
+    try:
+        driver.get(url)
+        driver.switch_to.frame('mainFrame')
+        WebDriverWait(driver, timeout=5).until(EC.presence_of_element_located((By.ID, 'whole-body')))
+        # time.sleep(5)
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        imgs = soup.find_all("a", class_="se-module-image-link __se_image_link __se_link")
+        imgcounts = len(imgs)
+        print(f'There\'re {imgcounts} photos found')
+        return imgcounts
+    except Exception as e:
+        print(f'Counting the photos got wrong! {e}')
 
 
-# find the counts of photos
-def Countsofphotos(url):
-    driver.get(url)
-    time.sleep(3)
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    imgs = soup.find_all("a", class_="se-module-image-link __se_image_link __se_link")
-    imgs_length = len(imgs)
-    return imgs_length
+def Downloadphotos(driver, imgcounts, url):
 
-def Downloadphotos(imgs_length):
     if not os.path.exists('images'):
         os.makedirs('images')
-    for img in range(imgs_length):
-        # the photoview url
-        photo_url = f'{url}&photoView={img}'
-        driver.get(photo_url)
-        time.sleep(3)
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
-        img_url = soup.find('img', class_='cpv__img cpv__fade').get('data-src')
+
+    for img in range(imgcounts):
+        try:
+            # the photoview url
+            photo_url = f'{url}?photoView={img}'
+            driver.get(photo_url)
+            driver.switch_to.frame('mainFrame')
+            time.sleep(3)
+            soup = BeautifulSoup(driver.page_source, 'html.parser')
+            img_url = unquote(soup.find('img', class_='cpv__img cpv__fade').get('data-src'))
+            # print(f'Original image url: {img_url}')
+        except Exception as e:
+            print(f'the {photo_url} was wrong! {e}')
+
+
+        file_name = unquote(img_url.split('?')[0].split('/')[-1].split('.')[0])
+        print(f'file name: {file_name}')
         file_extension = img_url.split('?')[0].split('.')[-1]
+        output_path = os.path.join('images', f'{file_name}.{file_extension}')
 
         download_photo = requests.get(img_url, headers=HEADERS, stream=True)
 
         if download_photo.status_code == 200:
-            with open(f"images/{img}.{file_extension}", 'wb') as file:
+            with open(output_path, 'wb') as file:
                 file.write(download_photo.content)
-            print(f"{img} downloading{"." * 20}\n")
+            print(f"{img + 1}/{imgcounts} downloading{"." * 20}\n")
         else:
-            print(f'download {img}.{file_extension} failed!')
+            print(f'download {file_name} failed!HTTP status code: {download_photo.status_code}')
+
     print('finished')
     driver.quit()
 
+
 if __name__ == '__main__':
-    Downloadphotos(Countsofphotos(url))
+
+    input_url = input('Enter the url you want to download pics: ')
+    pattern = r'https:\/\/blog.naver.com\/edament\/[0-9]{12}'
+    if re.fullmatch(pattern, input_url):
+        URL = input_url
+        print(f'The url you want to download is {URL}')
+    else:
+        URL = r'https://blog.naver.com/edament/'+input_url.split('No=')[1][:12]
+        print(f'The url you want to download is {input_url}')
+
+    webdriver = Iniwebdriver()
+
+    try:
+        Downloadphotos(webdriver, Countsofphotos(webdriver, URL), URL)
+    except Exception as e:
+        print(f'Something went wrong! {e}')
+    finally:
+        webdriver.quit()
